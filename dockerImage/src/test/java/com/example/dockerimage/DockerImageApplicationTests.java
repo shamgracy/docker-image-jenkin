661de87cb42d5d1887e84b7d3c02da519a5bb512
gracy <nithigracy31@gmail.com>
2022-08-18 14:40:35 +0530

    dockerimage

---
+++ dockerImage/src/test/java/com/example/dockerimage/DockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
