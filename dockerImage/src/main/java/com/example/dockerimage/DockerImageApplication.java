03d90c07f058b630e09813f9ff33ca0358dce25e
shamgracy <111081226+shamgracy@users.noreply.github.com>
2022-09-03 00:26:33 +0530

    Update DockerImageApplication.java

--- dockerImage/src/main/java/com/example/dockerimage/DockerImageApplication.java
+++ dockerImage/src/main/java/com/example/dockerimage/DockerImageApplication.java
@@ -1,13 +1,22 @@
-package com.example.dockerimage;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DockerImageApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerImageApplication.class, args);
 	}
-
+	
+	@GetMapping("/hi")
+	 public String getmessage()
+	 {
+		 
+		 return "Hi docker demo";
+	 }
 }
